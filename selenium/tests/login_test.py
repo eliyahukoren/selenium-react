08c4f0d560c54315c8abf8c62d330c83edfde9ae
eliyahu_k <elik@novelsat.com>
2025-06-26 14:35:04 +0300

    use PATH on docker and local for develop

--- selenium/tests/login_test.py
+++ selenium/tests/login_test.py
@@ -11,8 +11,11 @@ from selenium.webdriver.chrome.service import Service
 is_windows = platform.system() == "Windows"
 # Set path to local chromedriver
 # https://storage.googleapis.com/chrome-for-testing-public/138.0.7204.49/win32/chromedriver-win32.zip
-CHROMEDRIVER_PATH = "./driver/chromedriver" + (".exe" if is_windows else "")
-print(CHROMEDRIVER_PATH)
+if is_windows:
+    chromedriver_path = "./driver/chromedriver.exe"
+    service = Service(chromedriver_path)
+else:
+    service = Service()  # use chromedriver from PATH (for example installed via apt)
 
 # Read target URL from environment or fallback to localhost
 base_url = os.getenv("TARGET_HOST", "http://localhost:3002")
@@ -22,7 +25,6 @@ chrome_options = Options()
 chrome_options.add_argument("--log-level=3")  # suppress logs
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])  # suppress DevTools warning
 
-service = Service(CHROMEDRIVER_PATH)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 try:
