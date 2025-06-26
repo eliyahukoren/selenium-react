76f2e37822ad304b34fd16210934a3fd5bbd2456
eliyahu_k <elik@novelsat.com>
2025-06-26 16:19:02 +0300

    update reports path

--- selenium/run_tests.py
+++ selenium/run_tests.py
@@ -2,10 +2,11 @@ import unittest
 import xmlrunner
 
 if __name__ == '__main__':
+    output_dir = "/workspace/selenium-poc/selenium/reports"
     loader = unittest.TestLoader()
     suite = loader.discover(start_dir='tests', pattern='*_test.py')
 
-    runner = xmlrunner.XMLTestRunner(output='reports', verbosity=2)
+    runner = xmlrunner.XMLTestRunner(output=output_dir, verbosity=2)
     result = runner.run(suite)
 
     if not result.wasSuccessful():
