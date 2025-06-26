6d9fb967bda4b2b160d6b88d21403fcf79d8a9d1
eliyahu_k <elik@novelsat.com>
2025-06-26 16:06:23 +0300

    add unitest reporting

--- selenium/run_tests.py
+++ selenium/run_tests.py
@@ -1,10 +1,11 @@
 import unittest
+import xmlrunner
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
     suite = loader.discover(start_dir='tests', pattern='*_test.py')
 
-    runner = unittest.TextTestRunner(verbosity=2)
+    runner = xmlrunner.XMLTestRunner(output='reports', verbosity=2)
     result = runner.run(suite)
 
     if not result.wasSuccessful():
